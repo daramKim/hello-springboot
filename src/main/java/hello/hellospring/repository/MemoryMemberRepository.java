bad08c1e7fcadadc5d095790632d8c2b832d42e3
daramKim <cjw0672@gmail.com>
2021-08-29 19:08:32 +0900

    MemoryMemberRepository 구현

--- src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -1,10 +1,13 @@
 package hello.hellospring.repository;
 
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Stream;
 
 import hello.hellospring.domain.Member;
 
@@ -13,28 +16,39 @@ import hello.hellospring.domain.Member;
  * 실무에서는 ConcurrentHashMap, AtomicLong을 사용한다 함.
  */
 public class MemoryMemberRepository implements MemberRepository {
-	private static Map<Long, Member> store = new HashMap<>();
-	private static long id = 0;
+	private static Map<Long, Member> store = new HashMap<>(); // 임시 DB {메모리} 
+	private static long seq = 0L;
 	
 	@Override
 	public Member save(Member member) {
 		// TODO Auto-generated method stub
-		return null;
+		member.setId(++seq);
+		store.put(member.getId(), member);
+		return member;
 	}
+	
 	@Override
 	public Optional<Member> findById(Long id) {
 		// TODO Auto-generated method stub
-		return null;
+		return Optional.ofNullable(store.get(id));
 	}
+	
 	@Override
 	public Optional<Member> findByName(String name) {
 		// TODO Auto-generated method stub
-		return null;
+		Collection<Member> values = store.values();
+		Stream<Member> stream = values.stream();
+		Optional<Member> result = stream.filter(item -> item.getName().equals(name))
+				  	   					.findAny();
+		return result; 
 	}
+	
 	@Override
 	public List<Member> findAll() {
 		// TODO Auto-generated method stub
-		return null;
+		Collection<Member> values = store.values();
+		ArrayList<Member> list = new ArrayList<Member>(values);
+		return list;
 	}
 	
 }
