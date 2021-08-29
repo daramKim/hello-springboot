acf1c20bb5ace4c8ab130ed17d601ad19255762c
daramKim <cjw0672@gmail.com>
2021-08-29 10:41:12 +0900

    먼저 고객의 비즈니스를 파악하고,  도메인 객체 및 데이터저장소(DB 등)와 연계되어야 될 기능을 명세한다.  - DB가 정해지지 않았으므로 일단 interface로 정의해준다.

---
+++ src/main/java/hello/hellospring/repository/MemberRepository.java
@@ -0,0 +1,21 @@
+package hello.hellospring.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import hello.hellospring.domain.Member;
+
+public interface MemberRepository {
+	
+	/* DB가 뭐가 될지 모르니 interface로 정의하고
+	 * 해당 DB에서 사용될 기능들을 정해놓는다.
+	 */
+	
+	// 멤버 정보를 받아서 저장후, 저장된 회원정보 반환  
+	Member save(Member member);
+	
+	// 조회 - by{조건} || 전체
+	Optional<Member> findById(Long id);
+	Optional<Member> findByName(String name);
+	List<Member> findAll();
+}
