1df4f655022fd14437addfe6919d1f17f5189f6d
daramKim <cjw0672@gmail.com>
2021-08-29 19:20:43 +0900

    MemoryMemberRepositoryTest 생성. shortcut [ctrl+1]

---
+++ src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
@@ -0,0 +1,37 @@
+package hello.hellospring.repository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+
+
+/**
+ * MemoryMemberRepository의 테스트 클래스이다.    
+ * 네이밍 컨벤션: {테스트클래스명}Test.java 
+ * 테스트 하고자하는 곳에서 shortcut [ctrl+1] 을 누르면 편리하게 만들수 있다.
+ * 네이밍 컨벤션이 적용되고, 파일을 생성위치 및 생성할 테스트 메소드를 선택가능하다.
+ */
+class MemoryMemberRepositoryTest {
+
+	@Test
+	void testSave() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testFindById() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testFindByName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testFindAll() {
+		fail("Not yet implemented");
+	}
+
+}
