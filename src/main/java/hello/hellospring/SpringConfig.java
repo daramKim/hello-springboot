7e3854f8c52a37e916863c0db604e17876798679
daramKim <cjw0672@gmail.com>
2021-09-06 01:53:17 +0900

    DB 연결방식1 : jdbc api 사용  - 20년전 방법 개구닥다리

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,22 +1,40 @@
 package hello.hellospring;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import hello.hellospring.repository.JdbcMemberRepository;
 import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
 
 @Configuration
 public class SpringConfig {
+
+	private final DataSource dataSource;
 	
+	@Autowired
+	public SpringConfig(DataSource dataSource) {
+		this.dataSource = dataSource;
+	}
+
 	@Bean
 	public MemberService memberService() {
 		return new MemberService(memberRepository());
 	}
-	
-	@Bean 
+
+	/*
+	 * 개방-폐쇄 원칙(OCP, Open-Closed Principle)
+	 * - 확장에는 열려있고, 수정, 변경에는 닫혀있다.
+	 * 
+	 * 스프링의 DI(Dependencies Injection)을 사용
+	 * - 기존 코드를 전혀 손대지 않고, 설정만으로 구현 클래스를 변경할 수 있다.
+	 */
+	@Bean
 	public MemberRepository memberRepository() {
-		return new MemoryMemberRepository();
+		// return new MemoryMemberRepository();
+		return new JdbcMemberRepository(dataSource);
 	}
 }
---
+++ src/main/java/hello/hellospring/repository/JdbcMemberRepository.java
@@ -0,0 +1,179 @@
+package hello.hellospring.repository;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import javax.sql.DataSource;
+
+import org.springframework.jdbc.datasource.DataSourceUtils;
+
+import hello.hellospring.domain.Member;
+
+
+
+
+/**
+ * Long과 HashMap은 동시성 문제가 고려되어 있지 않다고 함. 실무에서는 ConcurrentHashMap, AtomicLong을
+ * 사용한다 함.
+ */
+
+public class JdbcMemberRepository implements MemberRepository {
+
+	// dataSource 는 applcation.properties 에서 등록된 정보임.
+	// applcation.properties 에 datasource 관련 설정을 해두면 spring이 자동으로 bean(dataSource)으로
+	// 등록해준다.
+	private final DataSource dataSource;
+
+	// 그리고 생성자를 이용해 외부에서 주입받게 한다. 이는 이전 MemoryRepository와 같은방식인, Spring Config에서
+	// 주입해주는 방식으로 한다.
+	public JdbcMemberRepository(DataSource dataSource) {
+		this.dataSource = dataSource;
+	}
+
+	@Override
+	public Member save(Member member) {
+		String sql = "insert into member(name) values(?)";
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+
+		try {
+			conn = getConnection();
+			pstmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+			pstmt.setString(1, member.getName());
+			pstmt.executeUpdate();
+			rs = pstmt.getGeneratedKeys();
+			if (rs.next()) {
+				member.setId(rs.getLong(1));
+			} else {
+				throw new SQLException("id 조회 실패");
+			}
+			return member;
+		} catch (Exception e) {
+			throw new IllegalStateException(e);
+		} finally {
+			close(conn, pstmt, rs);
+		}
+	}
+
+	@Override
+	public Optional<Member> findById(Long id) {
+		String sql = "select * from member where id = ?";
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			conn = getConnection();
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setLong(1, id);
+			rs = pstmt.executeQuery();
+
+			if (rs.next()) {
+				Member member = new Member();
+				member.setId(rs.getLong("id"));
+				member.setName(rs.getString("name"));
+				return Optional.of(member);
+			} else {
+				return Optional.empty();
+			}
+		} catch (Exception e) {
+			throw new IllegalStateException(e);
+		} finally {
+			close(conn, pstmt, rs);
+		}
+	}
+
+	@Override
+	public List<Member> findAll() {
+		String sql = "select * from member";
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			conn = getConnection();
+			pstmt = conn.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+			List<Member> members = new ArrayList<>();
+			while (rs.next()) {
+				Member member = new Member();
+				member.setId(rs.getLong("id"));
+				member.setName(rs.getString("name"));
+				members.add(member);
+			}
+
+			return members;
+		} catch (Exception e) {
+			throw new IllegalStateException(e);
+		} finally {
+			close(conn, pstmt, rs);
+		}
+	}
+
+	@Override
+	public Optional<Member> findByName(String name) {
+		String sql = "select * from member where name = ?";
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			conn = getConnection();
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, name);
+			rs = pstmt.executeQuery();
+			if (rs.next()) {
+				Member member = new Member();
+				member.setId(rs.getLong("id"));
+				member.setName(rs.getString("name"));
+				return Optional.of(member);
+			}
+			return Optional.empty();
+		} catch (Exception e) {
+			throw new IllegalStateException(e);
+		} finally {
+			close(conn, pstmt, rs);
+		}
+	}
+
+	
+	
+	
+	private Connection getConnection() {
+		return DataSourceUtils.getConnection(dataSource);
+	}
+
+	private void close(Connection conn, PreparedStatement pstmt, ResultSet rs) {
+		try {
+			if (rs != null) {
+				rs.close();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		try {
+			if (pstmt != null) {
+				pstmt.close();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		try {
+			if (conn != null) {
+				close(conn);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void close(Connection conn) throws SQLException {
+		DataSourceUtils.releaseConnection(conn, dataSource);
+	}
+
+
+}
