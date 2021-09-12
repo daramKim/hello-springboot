843aa1a93bff1ce5cef8b560dbe6a9769895e7c8
daramKim <cjw0672@gmail.com>
2021-09-12 23:49:24 +0900

    Spring Data JPA
    interface만으로 repository를 구현가능... data jpa가 자동으로 bean으로 등록해준다.
    
    실무에서는 JPA와 스프링 데이터 JPA를 기본으로 사용하고, 복잡한 동적 쿼리는 Querydsl이라는
    라이브러리를 사용하면 된다. Querydsl을 사용하면 쿼리도 자바 코드로 안전하게 작성할 수 있고, 동적
    쿼리도 편리하게 작성할 수 있다. 이 조합으로 해결하기 어려운 쿼리는 JPA가 제공하는 네이티브 쿼리를
    사용하거나, 앞서 학습한 스프링 JdbcTemplate를 사용하면 된다.

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -14,15 +14,13 @@ import hello.hellospring.service.MemberService;
 @Configuration
 public class SpringConfig {
 
-	private final DataSource dataSource;
-	private final EntityManager em;
+	 @Autowired
+	 private final MemberRepository memberRepository;
 	
-	@Autowired
-	public SpringConfig(DataSource dataSource, EntityManager em) {
-		this.dataSource = dataSource;
-		this.em = em;
-	}
-
+	 public SpringConfig(MemberRepository memberRepository) {
+		 this.memberRepository = memberRepository;
+	 }
+	 
 	@Bean
 	public MemberService memberService() {
 		return new MemberService(memberRepository());
